8bf51b813b519a299244b995d4e37ca3edae558f
Marcel Saegebarth <marcel.saegebarth@dci-student.org>
2024-12-10 13:06:53 +0100

    Implement account interface

--- src/main/java/org/dcistudent/banking/interfaces/models/AccountInterface.java
+++ src/main/java/org/dcistudent/banking/interfaces/models/AccountInterface.java
@@ -1,6 +1,18 @@
 package org.dcistudent.banking.interfaces.models;
 
 public interface AccountInterface {
+    public String getId();
+    public void setId(String id);
+    public String getCustomerId();
+    public void setCustomerId(String customerId);
+    public Integer getPin();
+    public void setPin(Integer pin);
+    public Integer getAccountType();
+    public void setAccountType(Integer accountType);
+    public Double getBalance();
+    public void setBalance(Double balance);
+    public Integer getOverdraftCount();
+    public void setOverdraftCount(Integer overdraftCount);
     public AccountInterface withdraw(Double amount);
     public AccountInterface deposit(Double amount);
 }
