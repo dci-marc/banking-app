c3562f4d990fc20f178bc992880548801bb4990f
Marcel Saegebarth <marcel.saegebarth@dci-student.org>
2024-12-12 11:09:37 +0100

    Enforce `toString()` by entity interface

--- src/main/java/org/dcistudent/banking/interfaces/entities/EntityInterface.java
+++ src/main/java/org/dcistudent/banking/interfaces/entities/EntityInterface.java
@@ -3,4 +3,5 @@ package org.dcistudent.banking.interfaces.entities;
 public interface EntityInterface {
     public String getId();
     public void setId(String id);
+    public String toString();
 }
